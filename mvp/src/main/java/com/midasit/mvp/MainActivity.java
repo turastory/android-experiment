b9ac6663d19a2972c6339dc6f76f36e99be35a25
nyh0111 <nyh0111@midasit.com>
2018-03-29 19:24:26 +0900

    Add new module - mvp

---
+++ mvp/src/main/java/com/midasit/mvp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.midasit.mvp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nyh0111 on 2018-03-29.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

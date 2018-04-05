3a2694efd127178249b32b0f3fc78297488002c9
nyh0111 <nyh0111@midasit.com>
2018-04-05 16:41:06 +0900

    Add drawing module

---
+++ drawing/src/main/java/teamidus/com/drawing/MainActivity.java
@@ -0,0 +1,18 @@
+package teamidus.com.drawing;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nyh0111 on 2018-04-05.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

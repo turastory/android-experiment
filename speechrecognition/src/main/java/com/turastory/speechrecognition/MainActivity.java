a003651c5d8e7f29f0761e9e60ff81aa537bb086
tura <soldier4443@gmail.com>
2018-04-23 19:35:37 +0900

    Extract Handler

--- speechrecognition/src/main/java/com/turastory/speechrecognition/MainActivity.java
+++ speechrecognition/src/main/java/com/turastory/speechrecognition/MainActivity.java
@@ -1,57 +1,43 @@
 package com.turastory.speechrecognition;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
-
-import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     
-    private static final int REQUEST_SPEECH_RECOGNITION = 100;
-    
     @BindView(R.id.text_view)
     TextView textView;
+    
+    private SpeechRecognitionHandler speechRecognizer;
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        
-        findViewById(R.id.button).setOnClickListener(v -> {
-            Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-            intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getPackageName());
-            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ko-KR");
-            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "말을 하세요.");
-            startActivityForResult(intent, REQUEST_SPEECH_RECOGNITION);
-        });
+    
+        speechRecognizer = SpeechRecognitionHandler.create("ko-KR")
+            .setPackageName(getPackageName())
+            .setAllSpeechListener(results -> {
+                for (String result : results)
+                    Log.d("Test", result);
+            })
+            .setSpeechListener(result ->
+                runOnUiThread(() -> textView.setText(result)));
+    
+        findViewById(R.id.button).setOnClickListener(v ->
+            speechRecognizer.startSpeechRecognition(this));
     }
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        
-        if (requestCode == REQUEST_SPEECH_RECOGNITION) {
-            if (resultCode == Activity.RESULT_OK && data != null) {
-                ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                
-                if (results != null) {
-                    for (String result : results) {
-                        Log.d("Test", result);
-                    }
-                    
-                    if (results.size() > 0)
-                        textView.setText(results.get(0));
-                }
-            }
-        }
+        speechRecognizer.handleActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ speechrecognition/src/main/java/com/turastory/speechrecognition/SpeechRecognitionHandler.java
@@ -0,0 +1,93 @@
+package com.turastory.speechrecognition;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+
+import java.util.ArrayList;
+
+/**
+ * Created by tura on 2018-04-23.
+ * <p>
+ * Handling Google's Speech recognition feature.
+ */
+public class SpeechRecognitionHandler {
+    
+    private static final int REQUEST_SPEECH_RECOGNITION = 100;
+    
+    private String packageName;
+    private String language;
+    private String promptMessage = "말을 하세요.";
+    private SpeechListener speechListener;
+    private AllSpeechListener allSpeechListener;
+    
+    private SpeechRecognitionHandler(String language) {
+        this.language = language;
+    }
+    
+    public static SpeechRecognitionHandler create(String language) {
+        return new SpeechRecognitionHandler(language);
+    }
+    
+    public SpeechRecognitionHandler setPackageName(String packageName) {
+        this.packageName = packageName;
+        return this;
+    }
+    
+    public SpeechRecognitionHandler setPromptMessage(String promptMessage) {
+        this.promptMessage = promptMessage;
+        return this;
+    }
+    
+    public SpeechRecognitionHandler setSpeechListener(SpeechListener onResultConsumer) {
+        this.speechListener = onResultConsumer;
+        return this;
+    }
+    
+    public SpeechRecognitionHandler setAllSpeechListener(AllSpeechListener allSpeechListener) {
+        this.allSpeechListener = allSpeechListener;
+        return this;
+    }
+    
+    public void startSpeechRecognition(Activity activity) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, packageName == null ? activity.getPackageName() : packageName);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, language == null ? provideDefaultLanguage() : language);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, promptMessage);
+        activity.startActivityForResult(intent, REQUEST_SPEECH_RECOGNITION);
+    }
+    
+    private boolean provideDefaultLanguage() {
+        return false;
+    }
+    
+    public void handleActivityResult(int requestCode, int resultCode, Intent data) {
+        if (speechListener == null && allSpeechListener == null) {
+            Log.d("SpeechRecognition", "Please provide result consumer for receive.");
+            return;
+        }
+        
+        if (requestCode == REQUEST_SPEECH_RECOGNITION) {
+            if (resultCode == Activity.RESULT_OK && data != null) {
+                ArrayList<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                
+                if (results != null) {
+                    allSpeechListener.onResults(results);
+                    
+                    if (results.size() > 0)
+                        if (speechListener != null)
+                            speechListener.onResult(results.get(0));
+                }
+            }
+        }
+    }
+    
+    public interface SpeechListener {
+        void onResult(String result);
+    }
+    
+    public interface AllSpeechListener {
+        void onResults(ArrayList<String> results);
+    }
+}
